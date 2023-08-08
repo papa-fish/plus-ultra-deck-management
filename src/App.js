d8fc0459cec6a37add098dca96b1c140113b6607
Luke Moore <Lukechristophermoore@gmail.com>
2023-08-08 15:43:55 +1000

    react-router refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import { getUser } from './utilities/users_service';
+import { Link, Route, Routes } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 
 import './App.css';
+
+function HomePage() {
+  return(
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
 
 function App() {
 
@@ -22,12 +30,29 @@ function App() {
 
   return (
     <div className="App">
-      <HomePage 
-        user={user}
-        onLogout={logout}
-      />
-      <LoginPage onLogin={login} />
-      <SignUpPage />
+
+    <nav className='nav-bar'>
+      <Link to="/">Home</Link>
+      <Link to="/decks">Decks</Link>
+      <Link to="/checklist">Checklist</Link>
+      <Link to="/currentdeck">Current Deck</Link>
+      { user ? <>
+        <p>Logged in as: {user.email}</p>
+        <Link onClick={logout} to="/">Logout</Link> </> :
+        <Link to="/users/login">Login</Link> 
+      }
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/users/login" element={<LoginPage onLogin={login}/>} />
+      <Route path="/users/signup" element={<SignUpPage />} />
+    </Routes>
+
+    <footer>
+      &copy; {new Date().getFullYear()}
+    </footer>
+
     </div>
   );
 }
